5d18bad037d45d1b8e234af6e05b4ac76deb3cd6
Andrew Gasson <agasson@red-elvis.net>
2019-02-26 08:06:03 +1300

    initial core of doom

---
+++ init.el
@@ -0,0 +1,26 @@
+;;; init.test.el -- for automated unit tests -*- lexical-binding: t; -*-
+
+(require 'core (concat user-emacs-directory "core/core"))
+
+(doom! :feature
+;       evil
+       workspaces
+
+       :completion
+       company
+
+       :ui
+       doom-dashboard
+
+       :tools
+       password-store
+
+       :lang
+       org
+ ;      web
+
+       :private
+       agasson)
+
+(provide 'init)
+;;; init/el ends here
